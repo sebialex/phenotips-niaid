c508be0d243ebb23588d0ddb2dac199738cd3370
Sebastian Stanisor <sebialex@gmail.com>
2016-07-28 18:39:25 -0400

    bacup

--- past-workup/ui/grab-xars.groovy
+++ past-workup/ui/grab-xars.groovy
@@ -5,4 +5,27 @@
  * See the NOTICE file distributed with this work for additional
  * information regarding copyright ownership.
  */
-println "hello"
+println "TESTING NIAID HAHAHAHAHAHA"
+
+
+import groovy.io.FileType
+
+def list = []
+
+def dir = new File("./")
+dir.eachFileRecurse (FileType.FILES) { file ->
+    list << file
+}
+
+println list
+
+def url = 'http://localhost:8080/export/PhenoTips/UIX_Field__past-workup?format=xar&name=PhenoTips.UIX_Field__past-workup&pages=xwiki%3APhenoTips.UIX_Field__past-workup'
+def file = new File('target/test.xar').newOutputStream()
+URLConnection urlConnection = new URL(url).openConnection();
+String userCredentials = "Admin:admin";
+String basicAuth = "Basic " + new String(userCredentials.bytes.encodeBase64().toString());
+urlConnection.setRequestProperty ("Authorization", basicAuth);
+file << urlConnection.getContent();
+
+
+
